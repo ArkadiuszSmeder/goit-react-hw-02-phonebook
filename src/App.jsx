25aa69d91aef8d178ae7c1e83aa730cd7d20b7d1
ArkadiuszSmeder <arkadiusz.smeder@gmail.com>
2024-04-25 13:42:58 +0200

    try

--- src/App.jsx
+++ src/App.jsx
@@ -52,7 +52,7 @@ function App() {
         <h1>Phonebook</h1>
         <Input type="text" label='Name' name='name' value={userData.name} onChange={onChange} pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$" title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan" required/>
         <Input type="tel" label='Number' name='number' value={userData.number} onChange={onChange} pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}" title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +" required/>
-        <button type="submit">Add contact</button>
+        <button type="submit">Add contacty</button>
         <Filter type="text" label='Find contacts by name' name='filter' value={userData.filter} onChange={onChange}/>
         <h1>Contacts</h1>
         <Contacts contacts={filteredContacts}/>
